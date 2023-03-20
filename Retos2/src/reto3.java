1851bdc523b6683a6429bc27e99a3724c8012bb8
DayanaVarg <128272265+DayanaVarg@users.noreply.github.com>
2023-03-19 22:19:35 -0500

    actualizacion
    se le agrega un system.out.print dentro del segundo ciclo for

--- Retos2/src/reto3.java
+++ Retos2/src/reto3.java
@@ -3,10 +3,10 @@ public class reto3 {
     public static void main(String[] args) {
         Scanner lect=new Scanner (System.in);
         String [][] matriz;
-        matriz=new String[7][3];
+        matriz=new String[4][4];
     
-        for (int fila=0;fila<7;fila++) {
-            for (int columna=0;columna<3;columna++) {
+        for (int fila=0;fila<4;fila++) {
+            for (int columna=0;columna<4;columna++) {
                 System.out.println("Digite el nombre y precio del producto en la posición: "+fila+columna);
                 matriz[fila][columna]=lect.nextLine();
                 
@@ -14,13 +14,14 @@ public class reto3 {
         } 
 
         System.out.println("");
-        for (int fila=0;fila<7;fila++) {
-            for (int columna=0;columna<3;columna++) {
+        for (int fila=0;fila<4;fila++) {
+            for (int columna=0;columna<4;columna++) {
             System.out.print(fila);
             System.out.print(columna+" ");
             System.out.print(matriz[fila][columna]+"\t");
-            System.out.print("|");
+            System.out.print(" ");
             }
+            System.out.println("|");
         }
     lect.close(); 
     }
